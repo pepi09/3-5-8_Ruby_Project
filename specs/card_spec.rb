105ea3add81ef9e9611d99a363b29467d39a7517
pepi09 <pepa.p.simeonova@abv.bg>
2016-02-11 22:24:17 +0200

    added specs for card

--- specs/card_spec.rb
+++ specs/card_spec.rb
@@ -19,4 +19,42 @@ describe Card do
 
     expect(card.humanize).to eq "Q of hearts"
   end
+
+  context 'comparing cards with operator <=>' do
+    before(:all) do
+      @card1 = Card.new('club', 'J')
+
+    end
+
+    it 'returns 0 for equal cards' do
+      expect(Card.new('club', 'J') <=> @card1).to eq 0
+    end
+
+    it 'returns 1 for when first card is bigger' do
+      expect(Card.new('club', 'Q') <=> @card1).to eq 1
+    end
+
+    it 'returns -1 for when first card is smaller' do
+      expect(Card.new('club', '9') <=> @card1).to eq -1
+    end
+  end
+
+  context 'comparing cards with operators <, >, ==' do
+    before(:all) do
+      @card1 = Card.new('club', 'J')
+
+    end
+
+    it 'returns 0 for equal cards' do
+      expect(Card.new('club', 'J') == @card1).to eq true
+    end
+
+    it 'returns 1 for when first card is bigger' do
+      expect(Card.new('club', 'Q') > @card1).to eq true
+    end
+
+    it 'returns -1 for when first card is smaller' do
+      expect(Card.new('club', '9') < @card1).to eq true
+    end
+  end
 end
